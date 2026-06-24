a4bae769389fdbf6a2d46507f52a6208cd731510
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-24 10:08:09 +0700

    Update and rename config.js to worker: node bot.js

--- config.js
+++
@@ -1,9 +0,0 @@
-export const CONFIG = {
-  BOT_TOKEN: "8314379349:AAF8uX_hHjsuW8VH9af3jrKbyXyMspEsL5Q",
-
-  ADMINS: [1305881282],
-
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz1_m7nbHX7dzx3AkmiCCsrAjmh2kRRUwHQAHp3recN4x6-Ru6ICfWCySh68gBJyDz9/exec",
-
-  BOT_LINK: "https://t.me/AITOOLSIGNAL_BOT?start=signal"
-};
