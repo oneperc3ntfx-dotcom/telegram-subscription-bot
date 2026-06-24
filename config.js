7aba1689c7fc218a1d23434d5ff091ded1ebac6c
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-24 09:39:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const CONFIG = {
 
   ADMINS: [1305881282],
 
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyDWhOFRO8T0PL2nZOzYAv0KaXIZxBAYfmBN0rVbuyNOGni6M84tdec-jp0Rp3gf2Ji/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw7JJRafyw9DQQU48yuZXERc52xakGnxaH5wepo_ipimlq_DlnDAQFjhmG9GnocDgeO/exec",
 
   BOT_LINK: "https://t.me/AITOOLSIGNAL_BOT?start=signal"
 };
