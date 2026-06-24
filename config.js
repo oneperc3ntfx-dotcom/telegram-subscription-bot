8cd415c6f97a100163edd701d8543c642cc4c747
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-24 09:59:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const CONFIG = {
-  BOT_TOKEN: "8314379349:AAEArZftvYB6tXRV6fg4uN3p9i82t6U1YOI",
+  BOT_TOKEN: "8314379349:AAF8uX_hHjsuW8VH9af3jrKbyXyMspEsL5Q",
 
   ADMINS: [1305881282],
 
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzW5qvrY5XhgYPAnVjQpV3j5GEK36UPKm5jhSTAShuB0jxRi4ZjCuSVyjf6SHcyOi3x/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz1_m7nbHX7dzx3AkmiCCsrAjmh2kRRUwHQAHp3recN4x6-Ru6ICfWCySh68gBJyDz9/exec",
 
   BOT_LINK: "https://t.me/AITOOLSIGNAL_BOT?start=signal"
 };
